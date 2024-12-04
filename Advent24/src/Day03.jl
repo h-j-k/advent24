7e6f72e32b83bff9056789e6ea88d5e884e8a7a5
H-J-K <han.junkwang@gmail.com>
2024-12-04 23:34:51 +0800

    Day 3 Part 2

--- Advent24/src/Day03.jl
+++ Advent24/src/Day03.jl
@@ -1,3 +1,3 @@
-process(regex::Regex, input::String) = sum(map(m -> reduce(*, parse.(Int, m.captures)), eachmatch(regex, input)))
+part1(input::String) = sum(map(m -> reduce(*, parse.(Int, m.captures)), eachmatch(r"mul\((\d{1,3}),(\d{1,3})\)", input)))
 
-part1(input::String) = process(r"mul\((\d{1,3}),(\d{1,3})\)", input)
+part2(input::String) = part1(join(split(input, r"don't\(\).*?(do\(\)|$)")))

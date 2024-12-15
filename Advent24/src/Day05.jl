1bd1d8620957eeb97bb6ef6e8e66f00035b57622
H-J-K <han.junkwang@gmail.com>
2024-12-16 01:25:48 +0800

    Day 5 part 1 refactoring.

--- Advent24/src/Day05.jl
+++ Advent24/src/Day05.jl
@@ -2,10 +2,10 @@ include("Util.jl")
 
 iscorrect(rulegroups) = (pages -> all(((i, page),) -> all(v -> v in get(rulegroups, page, Set()), pages[i + 1:end]), enumerate(pages)))
 
-middlesum(updates) = map(update -> parse(Int, update[trunc(Int, (length(update) + 1) / 2)]), updates)
+middlesum(updates) = map(update -> parse(Int, update[trunc(Int, (length(update) + 1) / 2)]), updates) |> sum
 
 function part1(input)
     (rules, updates) = partitionby(input, r -> r == "")
     rulegroups = mergewith(union, map(rule -> let (k, v) = split(rule, "|"); Dict(k => Set([v])); end, rules)...)
-    filter(iscorrect(rulegroups), map(update -> split(update, ","), updates)) |> middlesum |> sum
+    filter(iscorrect(rulegroups), map(update -> split(update, ","), updates)) |> middlesum
 end

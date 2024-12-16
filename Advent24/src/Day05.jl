2695b6ab8a7bc418bf5a6a199d28140dc037d379
H-J-K <han.junkwang@gmail.com>
2024-12-16 10:22:17 +0800

    Day 5 refactoring.

--- Advent24/src/Day05.jl
+++ Advent24/src/Day05.jl
@@ -1,20 +1,23 @@
 include("Util.jl")
 
-iscorrect(rulegroups) = (pages -> all(((i, page),) -> all(v -> v in get(rulegroups, page, Set()), pages[i + 1:end]), enumerate(pages)))
+process(input) = let (rules, updates) = partitionby(input, r -> r == "")
+    (mergewith(union, map(rule -> let (k, v) = split(rule, "|"); Dict(k => Set([v])); end, rules)...), 
+    map(update -> split(update, ","), updates))
+end
+
+iscorrect(rules) = (pages -> all(((i, page),) -> all(v -> v in get(rules, page, Set()), pages[i + 1:end]), enumerate(pages)))
 
 middlesum(updates) = map(update -> parse(Int, update[trunc(Int, (length(update) + 1) / 2)]), updates) |> sum
 
 function part1(input)
-    (rules, updates) = partitionby(input, r -> r == "")
-    rulegroups = mergewith(union, map(rule -> let (k, v) = split(rule, "|"); Dict(k => Set([v])); end, rules)...)
-    filter(iscorrect(rulegroups), map(update -> split(update, ","), updates)) |> middlesum
+    (rules, updates) = process(input)
+    filter(iscorrect(rules), updates) |> middlesum
 end
 
 function part2(input)
-    (rules, updates) = partitionby(input, r -> r == "")
-    rulegroups = mergewith(union, map(rule -> let (k, v) = split(rule, "|"); Dict(k => Set([v])); end, rules)...)
-    map(updates -> 
-        let x = trunc(Int, (length(updates) - 1) / 2); filter(page -> count(v -> v in get(rulegroups, page, Set()), updates) == x, updates) end, 
-        filter(!iscorrect(rulegroups), map(update -> split(update, ","), updates))
+    (rules, updates) = process(input)
+    map(update -> 
+        let x = trunc(Int, (length(update) - 1) / 2); filter(page -> count(v -> v in get(rules, page, Set()), update) == x, update) end, 
+        filter(!iscorrect(rules), updates)
     ) |> middlesum
 end
